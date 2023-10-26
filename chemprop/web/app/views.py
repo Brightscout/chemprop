100bdaf77ab908c9462e1e076cfed5a966a839a5
Kyle Swanson <swansonk.14@gmail.com>
2023-10-25 23:00:29 -0700

    Computing physicochemical properties

--- chemprop/web/app/views.py
+++ chemprop/web/app/views.py
@@ -22,6 +22,10 @@ import seaborn as sns
 from chemfunc.molecular_fingerprints import compute_fingerprints
 from flask import json, jsonify, redirect, render_template, request, send_file, send_from_directory, url_for
 from rdkit import Chem
+from rdkit.Chem.Crippen import MolLogP
+from rdkit.Chem.Descriptors import MolWt
+from rdkit.Chem.QED import qed
+from rdkit.Chem.rdMolDescriptors import CalcNumAtomStereoCenters, CalcNumHBA, CalcNumHBD, CalcTPSA
 from scipy.stats import gaussian_kde
 from tqdm import tqdm
 from werkzeug.utils import secure_filename
@@ -447,6 +451,55 @@ def predict_all_models(
     return all_task_names, all_preds
 
 
+def lipinski_rule_of_five(mol: Chem.Mol) -> float:
+    """Determines how many of the Lipinski rules are satisfied by the molecule.
+
+    :param mol: An RDKit molecule.
+    :return: The number of Lipinski rules satisfied by the molecule.
+    """
+    return float(sum([
+        MolWt(mol) <= 500,
+        MolLogP(mol) <= 5,
+        CalcNumHBA(mol) <= 10,
+        CalcNumHBD(mol) <= 5
+    ]))
+
+
+def compute_physicochemical_properties(
+        smiles: list[str],
+) -> tuple[list[str], list[list[float]]]:
+    """Compute physicochemical properties for a list of molecules.
+
+    :param smiles: A list of SMILES.
+    :return: A tuple containing a list of property names and a list of properties (num_molecules, num_properties).
+    """
+    # Compute RDKit molecules
+    # TODO: avoid recomputing the RDKit molecules if they have to be computed for Chemprop anyway
+    # TODO: handle invalid molecules
+    mols = [Chem.MolFromSmiles(smile) for smile in smiles]
+
+    # Set up physicochemical property functions
+    property_functions = {
+        "molecular_weight": MolWt,
+        "logP": MolLogP,
+        "hydrogen_bond_acceptors": CalcNumHBA,
+        "hydrogen_bond_donors": CalcNumHBD,
+        "Lipinski": lipinski_rule_of_five,
+        "QED": qed,
+        "stereo_centers": CalcNumAtomStereoCenters,
+        "tpsa": CalcTPSA,
+    }
+
+    # Compute properties
+    property_names = list(property_functions)
+    properties = [
+        [property_function(mol) for mol in mols]
+        for property_name, property_function in property_functions.items()
+    ]
+
+    return property_names, properties
+
+
 def create_drugbank_reference_plot(
         preds_df: pd.DataFrame,
         x_task: str = 'HIA_Hou',
@@ -560,6 +613,9 @@ def predict():
     # Make predictions
     task_names, preds = predict_all_models(smiles=smiles)
     num_tasks = len(task_names)
+
+    # TODO: Display physicochemical properties (and compare to DrugBank)
+    physchem_names, physchem_preds = compute_physicochemical_properties(smiles=smiles)
 
     # Compute DrugBank percentiles
     preds_numpy = np.array(preds).transpose()  # (num_tasks, num_molecules)
